f05c052f5bcb58f2bc773a088d02bd0c6d52b515
miguel <miguel@miguel-HP-Pavilion-dv5-Notebook-PC>
2011-10-07 16:06:15 -0500

    se crea una clase separada para la encriptacion

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -1,16 +1,12 @@
 package controller;
 
-
 import model.Parkway;
 import view.LoginView;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import DAO.EmployeeJpaController;
 import Entity.Employee;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.List;
-import java.util.logging.Logger;
 
 /**
  *
@@ -27,39 +23,10 @@ public class LoginController {
         this.persistence_factory = Persistence.createEntityManagerFactory("ParkingSoftwarePU");
 
     }
-    public static String byteArrToString(byte[] b){
-   String res = null;
-   StringBuffer sb = new StringBuffer(b.length * 2);
-   for (int i = 0; i < b.length; i++){
-      int j = b[i] & 0xff;
-      if (j < 16) {
-         sb.append('0');
-      }
-      sb.append(Integer.toHexString(j));
-   }
-   res = sb.toString();
-   return res.toUpperCase();
-}
-    
-    private String security(String password){
-        try {
-            String passMD5=null;
-            byte[] passbyte=password.getBytes();
-            MessageDigest md = MessageDigest.getInstance("MD5");
-             
-            md.update(passbyte);
-            passMD5=byteArrToString(md.digest());
-                       
-            return passMD5;
-        } catch (NoSuchAlgorithmException ex) {
-            return "Error de seguridad, imposible converitr a MD5";
-        }
-        
-    }
-    
 
     public boolean verifyUser(String user, String password) {
         EmployeeJpaController ctrlemployee = new EmployeeJpaController(persistence_factory);
+        MD5Security security = new MD5Security();
         /* este codigo crea un nuevo usuario en la base de datos, 
          * desmarcarlo solo en caso que no se tengan usuarios creados con anterioridad
         Employee employee1=new Employee();
@@ -70,17 +37,13 @@ public class LoginController {
         employee1.setPassword(security("miguel"));
         employee1.setUser("miguel");
         ctrlemployee.create(employee1);*/
-        
-         
-        
-        
         List<Employee> listemployee = ctrlemployee.findEmployeeEntities();
         for (Employee employee : listemployee) {
-            System.out.println("1. "+employee.getUser()+" // "+user+"\n");
-                System.out.println("2. "+employee.getPassword()+" // "+security(password)+"\n");
-            
-            if (employee.getUser().equals(user) && employee.getPassword().equals(security(password))) {
-                
+            System.out.println("1. " + employee.getUser() + " // " + user + "\n");
+            System.out.println("2. " + employee.getPassword() + " // " + security.MD5Security(password) + "\n");
+
+            if (employee.getUser().equals(user) && employee.getPassword().equals(security.MD5Security(password))) {
+
                 return true;
             }
 
