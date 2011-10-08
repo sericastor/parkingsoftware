3247223cdb05095f48d587294631631c4a1c4cf3
r4wd3r <r4wd3r@r4wd3r-PC>
2011-10-08 18:42:32 -0500

    Implementaci?n de B?squedas de Operario, y Organizaci?n de Llamados a Controladores. Creaci?n de Clases Controladoras relacionadas a la Administraci?n del Sistema.

---
+++ src/controller/Administration/AdministrationController.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller.Administration;
+
+import controller.MainController;
+
+/**
+ *
+ * @author r4wd3r
+ */
+public class AdministrationController {
+    public AdministrationController(){
+        new AdministrateEmployeeController();
+    }
+    public void loadAdministrationData(){
+        initializeAdminEmployee();
+    }
+    public static void initializeAdminEmployee(){
+        MainController.getAdminView().getEmployeeList().setModel(AdministrateEmployeeController.TotalSearchOfEmployees());
+    }
+}
