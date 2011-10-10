57e183b185b1c2443985255e25fe927ccaa974b4
miguel <miguel@miguel-HP-Pavilion-dv5-Notebook-PC>
2011-10-10 13:28:57 -0500

    implementados manejo de skins

--- src/eParking/eParking.java
+++ src/eParking/eParking.java
@@ -7,6 +7,10 @@ package eParking;
 import controller.Administration.AdministrateEmployeeController;
 import controller.Administration.AdministrationController;
 import controller.MainController;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import view.LoginView;
 
 /**
@@ -14,7 +18,20 @@ import view.LoginView;
  * @author r4wd3r
  */
 public class eParking{ 
-    public static void main(String args[]){        
+    public static void main(String args[]) throws ClassNotFoundException{   
+        try {
+            //inserta la piel segun el sistema operativo
+            UIManager.setLookAndFeel(
+            UIManager.getSystemLookAndFeelClassName());
+            //inserta la piel de la libreria agregada "jtatoo" que contiene varios skins
+             //UIManager.setLookAndFeel("com.jtattoo.plaf.graphite.GraphiteLookAndFeel");
+        } catch (InstantiationException ex) {
+            Logger.getLogger(eParking.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(eParking.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(eParking.class.getName()).log(Level.SEVERE, null, ex);
+        }
         new MainController();
        // new Parkway();
         new AdministrationController();
