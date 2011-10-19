709646b602b2789f0453aa21ab78f924f5697d1c
miguel <miguel@miguel-PC>
2011-10-19 16:59:50 -0500

    super merge!

--- src/controller/Administration/EmployeeManagementController.java
+++ src/controller/Administration/EmployeeManagementController.java
@@ -1,88 +1,64 @@
-package controller;
+package controller.Administration;
 
-public class EmployeeManagmentController {
+public class EmployeeManagementController {
 
-    public EmployeeManagmentController() {
+    public EmployeeManagementController() {
     }
 
-    
-    
-    public static boolean validateID(String ID) {
-
-        int id;
-
-        try {
-            id = Integer.parseInt(ID);
-            //validar en la base de datos el ID
+    public static boolean validatePasswords(String pass, String confirm){
+        if(pass.equals(confirm)){
             return true;
-
-        } catch (Exception ex) {
-            //Mandar a la vista y mandar error
-            System.out.println("Ha ocurrido el siguiente error " + ex.getMessage());
         }
         return false;
-
     }
-
+    
+    public static boolean validateNotEmptyFields(String lastName, String name, String document, String user, String password){
+        if(lastName.isEmpty() || name.isEmpty() || document.isEmpty() || user.isEmpty() || password.isEmpty()){
+            return false;
+        }
+        return true;
+    }
+    
+    public static boolean validateAll(String lastName, String name, String document, String user, String password){
+        if(validateLastName(lastName) && validateName(name) && validateDocument(document) && validateUser(user) && validatePassword(password)){
+            return true;
+        }
+        return false;
+    }
+    
     public static boolean validateLastName(String lastName) {
-
         if (lastName.length() >= 2 && lastName.length() <= 25) {
-
             return true;
         }
-
-        //mandar error
         return false;
-
     }
 
     public static boolean validateName(String name) {
-
         if (name.length() > 1 && name.length() < 26) {
-
             return true;
         }
-
-        //mandar error
         return false;
-
     }
 
     public static boolean validateDocument(String document) {
-
         if (document.length() >= 6 && document.length() <= 15) {
-
             return true;
         }
-
-        //mandar error
         return false;
     }
-        
     
-
     public static boolean validateUser(String user) {
-
         if (user.length() >= 6 && user.length() <= 10) {
-
             return true;
         }
-
-        //mandar error
         return false;
-
     }
     
     public static boolean validatePassword(String password) {
-
         if (password.length() >= 4 && password.length() <= 10) {
-
             return true;
         }
-
-        //mandar error
         return false;
-
     }
     
     
