fe129440e2372397e8e9f6736539ee3c00dd188a
miguel <miguel@miguel-PC>
2011-10-18 22:52:00 -0500

    arreglados multiples bugs en administrar tarifas falta modificar sistema de busqueda de objetos a un query en el DAO de BandsRate

--- src/controller/Administration/AdministrateBandRates.java
+++ src/controller/Administration/AdministrateBandRates.java
@@ -51,16 +51,17 @@ public class AdministrateBandRates {
         return results;
     }
 
-    public static void updateRow(int row, String from, String to, String fraction, String value,VehicleType vehicletype) {
+    public static void updateRow(int row, String id, String from, String to, String fraction, String value,VehicleType vehicletype) {
         AllBandsRate = MainController.bandsRateJpaController.findBandsRateEntities();//actualiza listado
+        try {
         BandsRate aux = new BandsRate();
         aux.setFromm(Integer.parseInt(from));
         aux.setToo(Integer.parseInt(to));
         aux.setUnits(Integer.parseInt(fraction));
         aux.setUnitValue(Double.parseDouble(value));
         aux.setVehicletype(vehicleTypeIsSelected);
-        aux.setId(AllBandsRate.get(row).getId());
-        try {
+        aux.setId(Integer.parseInt(id));
+        
             MainController.bandsRateJpaController.edit(aux);
             String description = "from: " + AllBandsRate.get(row).getFromm() + " -> " + from
                     + " to:" + AllBandsRate.get(row).getToo() + " -> " + to
@@ -68,7 +69,10 @@ public class AdministrateBandRates {
                     + " value:" + AllBandsRate.get(row).getUnitValue() + " -> " + value;
             MainController.system.UpdateBandRate(aux.getId(), description);
             AllBandsRate = MainController.bandsRateJpaController.findBandsRateEntities();
-        } catch (NonexistentEntityException ex) {
+        } 
+        catch(NumberFormatException ex){
+            //error generado al tratar de hacer un parse con un string ""
+        }catch (NonexistentEntityException ex) {
             Logger.getLogger(AdministrateBandRates.class.getName()).log(Level.SEVERE, null, ex);
         } catch (Exception ex) {
             Logger.getLogger(AdministrateBandRates.class.getName()).log(Level.SEVERE, null, ex);
@@ -89,7 +93,7 @@ public class AdministrateBandRates {
 
     }
 
-    public static void rowIsEdited(int row, String from, String to, String fraction, String value,VehicleType vehicletype) {
+    public static void rowIsEdited(int row, String id,String from, String to, String fraction, String value,VehicleType vehicletype) {
         if (AllBandsRate.isEmpty() || AllBandsRate.size() <= row) {
             try {
                 createRow(row, from, to, fraction, value, vehicletype);
@@ -100,16 +104,16 @@ public class AdministrateBandRates {
             }
         }
         if (!String.valueOf(AllBandsRate.get(row).getFromm()).equals(from)) {
-            updateRow(row, from, to, fraction, value, vehicletype);
+            updateRow(row, id, from, to, fraction, value, vehicletype);
             return;
         } else if (!String.valueOf(AllBandsRate.get(row).getToo()).equals(to)) {
-            updateRow(row, from, to, fraction, value, vehicletype);
+            updateRow(row, id, from, to, fraction, value, vehicletype);
             return;
         } else if (!String.valueOf(AllBandsRate.get(row).getUnits()).equals(fraction)) {
-            updateRow(row, from, to, fraction, value, vehicletype);
+            updateRow(row, id, from, to, fraction, value, vehicletype);
             return;
         } else if (!String.valueOf(AllBandsRate.get(row).getUnitValue()).equals(value)) {
-            updateRow(row, from, to, fraction, value, vehicletype);
+            updateRow(row, id,  from, to, fraction, value, vehicletype);
             return;
         }
 
