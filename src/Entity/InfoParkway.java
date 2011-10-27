eda393ab2046057b14982aae740d3349dc8380c2
Martin Kanayet <Martin Kanayet@MartinKanayet>
2011-10-27 18:31:14 -0500

    Cositas Varias

--- src/Entity/InfoParkway.java
+++ src/Entity/InfoParkway.java
@@ -103,7 +103,7 @@ public class InfoParkway implements Serializable {
 
     @Override
     public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
+  
         if (!(object instanceof InfoParkway)) {
             return false;
         }
