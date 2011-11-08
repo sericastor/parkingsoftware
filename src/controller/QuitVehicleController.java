80ae73052dcb2de05b1b432ddb677ffa9a6ec31a
r4wd3r <r4wd3r@r4wd3r-PC>
2011-11-08 18:02:55 -0500

    C?lculo de Minutos Transcurridos entre Hora de Entrada y Hora de salida. Ese dato se imprime en consola cuando se da clic en "Facturar". Cambios Varios

--- src/controller/QuitVehicleController.java
+++ src/controller/QuitVehicleController.java
@@ -1,7 +1,33 @@
 package controller;
 
+import Entity.VehicleType;
+import java.util.Date;
+
 public class QuitVehicleController {
 
     public QuitVehicleController() {
+    }
+
+    public static long getDifferenceBetweenHours(Date entry, Date exit) {
+        long resultInMinutes = 0;
+        long miliseconds = exit.getTime() - entry.getTime();
+        long seconds = miliseconds / 1000;
+        long hours = seconds / 3600;
+        seconds -= hours * 3600;
+        long minutes = seconds / 60;
+        seconds -= minutes * 60;
+        resultInMinutes = resultInMinutes + hours*60;
+        resultInMinutes = resultInMinutes + minutes;
+        if (seconds >= 30){
+            resultInMinutes = resultInMinutes + 1;
+        }
+        return resultInMinutes;
+    }
+
+    public static double calculateCost(Date entry, Date exit, VehicleType vehicleType) {
+        double cost = 0;
+        long minutesParked = getDifferenceBetweenHours(entry, exit);
+        System.out.println(minutesParked);
+        return cost;
     }
 }
