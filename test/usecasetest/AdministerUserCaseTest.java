a2a85b115f71af0176af621ead2f28d866da2d15
Lore <Lore@dasalgadoc>
2011-12-29 12:37:56 -0500

    Caso de prueba 005-02 Completo.

--- test/usecasetest/AdministerUserCaseTest.java
+++ test/usecasetest/AdministerUserCaseTest.java
@@ -11,6 +11,8 @@ import javax.persistence.Persistence;
 import javax.persistence.EntityManagerFactory;
 import DAO.EmployeeJpaController;
 import controller.Administration.AdministrateEmployeeController;
+import controller.Administration.EmployeeManagementController;
+import controller.MainController;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -82,7 +84,6 @@ public class AdministerUserCaseTest {
     }
 
     @Test
-    // Thanks to bad initialization, this test must be changed
     public void searchAllEmployees() {
         DefaultListModel allEmployees = AdministrateEmployeeController.totalSearchOfEmployees();
         
@@ -114,31 +115,319 @@ public class AdministerUserCaseTest {
     }
     
     @Test
-    public void createAValidEmployee(){}
+    public void createAValidEmployee(){
+        setId(EmployeeManagementController.getNextID());
+        setName("Miguel");
+        setLastName("Diaz");
+        setDocument("123456");
+        setUser("mcdiaz");
+        setPassword("parquick");
+        setConfirmation("parquick");
+        setAdministrator(false);
+        setActive(true);
+        
+        assertEquals(getId(),4);
+        
+        EmployeeManagementController.saveNewEmployee(getId(), getLastName(), getName(), getDocument(), getUser(), getPassword(), getConfirmation(), isActive(), isAdministrator());
+        
+        assertTrue(EmployeeManagementController.validatePasswords(getPassword(), getConfirmation()));
+        assertTrue(EmployeeManagementController.validateNotEmptyFields(getLastName(), getName(), getDocument(), getUser(), getPassword()));
+        assertTrue(EmployeeManagementController.validateAll(getLastName(), getName(), getDocument(), getUser(), getPassword()));
+        
+        Employee lastEmployee = employeeJpaController.findEmployee(Long.valueOf(id));
+        
+        assertEquals(lastEmployee.getId(),4);
+        assertEquals(lastEmployee.getName(),"Miguel");
+        assertEquals(lastEmployee.getLastName(),"Diaz");
+        assertEquals(lastEmployee.getDocument(),"123456");
+        assertEquals(lastEmployee.getUser(),"mcdiaz");
+        assertEquals(lastEmployee.getPassword(),MainController.md5Security.MD5Security(getPassword()));
+        assertFalse(lastEmployee.isAdministrator());
+        assertTrue(lastEmployee.isIsActive());
+    }
     
     @Test
-    public void createAnEmployeeWithRepeatedUser(){}
+    public void createAnEmployeeWithRepeatedUser(){
+        setId(EmployeeManagementController.getNextID());
+        setName("Felipe");
+        setLastName("Castaño");
+        setDocument("123456");
+        setUser("srcastrot");
+        setPassword("parquick");
+        setConfirmation("parquick");
+        setAdministrator(false);
+        setActive(true);
+        
+        assertEquals(getId(),5);
+        
+        EmployeeManagementController.saveNewEmployee(getId(), getLastName(), getName(), getDocument(), getUser(), getPassword(), getConfirmation(), isActive(), isAdministrator());
+        
+        assertTrue(EmployeeManagementController.validatePasswords(getPassword(), getConfirmation()));
+        assertTrue(EmployeeManagementController.validateNotEmptyFields(getLastName(), getName(), getDocument(), getUser(), getPassword()));
+        assertTrue(EmployeeManagementController.validateAll(getLastName(), getName(), getDocument(), getUser(), getPassword()));
+        
+        assertNull(employeeJpaController.findEmployee(Long.valueOf(id)));
+    }
     
     @Test
-    public void createAnEmployeeWithNotEqualsPasswords(){}
+    public void createAnEmployeeWithNotEqualsPasswords(){
+        setId(EmployeeManagementController.getNextID());
+        setName("Felipe");
+        setLastName("Castaño");
+        setDocument("789012");
+        setUser("spipex");
+        setPassword("superpip");
+        setConfirmation("parquick");
+        setAdministrator(false);
+        setActive(true);
+        
+        assertEquals(getId(),5);
+        
+        EmployeeManagementController.saveNewEmployee(getId(), getLastName(), getName(), getDocument(), getUser(), getPassword(), getConfirmation(), isActive(), isAdministrator());
+        
+        assertFalse(EmployeeManagementController.validatePasswords(getPassword(), getConfirmation()));
+        
+        assertNull(employeeJpaController.findEmployee(Long.valueOf(id)));
+    }
     
     @Test
-    public void createAnEmployeeWithEmptyFields(){}
+    public void createAnEmployeeWithEmptyFields(){
+        setId(EmployeeManagementController.getNextID());
+        setName("Felipe");
+        setLastName("");
+        setDocument("");
+        setUser("spipex");
+        setPassword("parquick");
+        setConfirmation("parquick");
+        setAdministrator(false);
+        setActive(true);
+        
+        assertEquals(getId(),5);
+        
+        EmployeeManagementController.saveNewEmployee(getId(), getLastName(), getName(), getDocument(), getUser(), getPassword(), getConfirmation(), isActive(), isAdministrator());
+        
+        assertTrue(EmployeeManagementController.validatePasswords(getPassword(), getConfirmation()));
+        assertFalse(EmployeeManagementController.validateNotEmptyFields(getLastName(), getName(), getDocument(), getUser(), getPassword()));
+        
+        assertNull(employeeJpaController.findEmployee(Long.valueOf(id)));
+    }
     
     @Test
-    public void updateAnEmployeeSuccessful(){}
+    public void updateAnEmployeeSuccessful(){
+        // New Data
+        setName("Ricardo");
+        setPassword("ingesoft");
+        setConfirmation("ingesoft");
+        setAdministrator(true);
+        
+        Employee updateEmployee = employeeJpaController.findEmployee(Long.valueOf(3));
+       
+        assertEquals(updateEmployee.getId(),3);
+        assertEquals(updateEmployee.getName(),"Sebastian");
+        assertEquals(updateEmployee.getLastName(),"Castro");
+        assertEquals(updateEmployee.getDocument(),"257833");
+        assertEquals(updateEmployee.getUser(),"srcastrot");
+        assertEquals(updateEmployee.getPassword(),MainController.md5Security.MD5Security("castor"));
+        assertFalse(updateEmployee.isAdministrator());
+        assertTrue(updateEmployee.isIsActive());
+        
+        //Old Data
+        setId(updateEmployee.getId());
+        setLastName(updateEmployee.getLastName());
+        setDocument(updateEmployee.getDocument());
+        setUser(updateEmployee.getUser());
+        setActive(updateEmployee.isIsActive());
+        
+        EmployeeManagementController.updateOldEmployee(getId(), getLastName(), getName(), getDocument(), getUser(), getPassword(), getConfirmation(), isActive(), isAdministrator());
+        
+        assertTrue(EmployeeManagementController.validatePasswords(getPassword(), getConfirmation()));
+        assertTrue(EmployeeManagementController.validateNotEmptyFields(getLastName(), getName(), getDocument(), getUser(), getPassword()));
+        assertTrue(EmployeeManagementController.validateAll(getLastName(), getName(), getDocument(), getUser(), getPassword()));
+        
+        updateEmployee = employeeJpaController.findEmployee(Long.valueOf(3));
+        
+        assertEquals(updateEmployee.getId(),3);
+        assertEquals(updateEmployee.getName(),"Ricardo");
+        assertEquals(updateEmployee.getLastName(),"Castro");
+        assertEquals(updateEmployee.getDocument(),"257833");
+        assertEquals(updateEmployee.getUser(),"srcastrot");
+        assertEquals(updateEmployee.getPassword(),MainController.md5Security.MD5Security("ingesoft"));
+        assertTrue(updateEmployee.isAdministrator());
+        assertTrue(updateEmployee.isIsActive());
+    }
     
     @Test
-    public void updateAnEmployeeWithRepeatedUser(){}
+    public void updateAnEmployeeWithRepeatedUser(){
+        //New Data
+        setUser("mkanayet");
+        setPassword("castor");
+        setConfirmation("castor");
+        
+        Employee updateEmployee = employeeJpaController.findEmployee(Long.valueOf(3));
+        
+        assertEquals(updateEmployee.getId(),3);
+        assertEquals(updateEmployee.getName(),"Ricardo");
+        assertEquals(updateEmployee.getLastName(),"Castro");
+        assertEquals(updateEmployee.getDocument(),"257833");
+        assertEquals(updateEmployee.getUser(),"srcastrot");
+        assertEquals(updateEmployee.getPassword(),MainController.md5Security.MD5Security("ingesoft"));
+        assertTrue(updateEmployee.isAdministrator());
+        assertTrue(updateEmployee.isIsActive());
+        
+        //Old Data
+        setId(updateEmployee.getId());
+        setName(updateEmployee.getName());
+        setLastName(updateEmployee.getLastName());
+        setDocument(updateEmployee.getDocument());
+        setAdministrator(updateEmployee.isAdministrator());
+        setActive(updateEmployee.isIsActive());
+        
+        EmployeeManagementController.updateOldEmployee(getId(), getLastName(), getName(), getDocument(), getUser(), getPassword(), getConfirmation(), isActive(), isAdministrator());
+        
+        assertTrue(EmployeeManagementController.validatePasswords(getPassword(), getConfirmation()));
+        assertTrue(EmployeeManagementController.validateNotEmptyFields(getLastName(), getName(), getDocument(), getUser(), getPassword()));
+        assertTrue(EmployeeManagementController.validateAll(getLastName(), getName(), getDocument(), getUser(), getPassword()));
+        
+        updateEmployee = employeeJpaController.findEmployee(Long.valueOf(3));
+        
+        assertEquals(updateEmployee.getId(),3);
+        assertEquals(updateEmployee.getName(),"Ricardo");
+        assertEquals(updateEmployee.getLastName(),"Castro");
+        assertEquals(updateEmployee.getDocument(),"257833");
+        assertEquals(updateEmployee.getUser(),"srcastrot");
+        assertEquals(updateEmployee.getPassword(),MainController.md5Security.MD5Security("ingesoft"));
+        assertTrue(updateEmployee.isAdministrator());
+        assertTrue(updateEmployee.isIsActive());
+    }
     
     @Test
-    public void updateAnEmployeeWithNotEqualsPasswords(){}
+    public void updateAnEmployeeWithNotEqualsPasswords(){
+        //New Data
+        setPassword("sericastor");
+        setConfirmation("castor");
+        
+        Employee updateEmployee = employeeJpaController.findEmployee(Long.valueOf(3));
+        
+        assertEquals(updateEmployee.getId(),3);
+        assertEquals(updateEmployee.getName(),"Ricardo");
+        assertEquals(updateEmployee.getLastName(),"Castro");
+        assertEquals(updateEmployee.getDocument(),"257833");
+        assertEquals(updateEmployee.getUser(),"srcastrot");
+        assertEquals(updateEmployee.getPassword(),MainController.md5Security.MD5Security("ingesoft"));
+        assertTrue(updateEmployee.isAdministrator());
+        assertTrue(updateEmployee.isIsActive());
+        
+        //Old Data
+        setId(updateEmployee.getId());
+        setName(updateEmployee.getName());
+        setLastName(updateEmployee.getLastName());
+        setDocument(updateEmployee.getDocument());
+        setUser(updateEmployee.getUser());
+        setAdministrator(updateEmployee.isAdministrator());
+        setActive(updateEmployee.isIsActive());
+        
+        EmployeeManagementController.updateOldEmployee(getId(), getLastName(), getName(), getDocument(), getUser(), getPassword(), getConfirmation(), isActive(), isAdministrator());
+        
+        assertFalse(EmployeeManagementController.validatePasswords(getPassword(), getConfirmation()));
+        
+        updateEmployee = employeeJpaController.findEmployee(Long.valueOf(3));
+        
+        assertEquals(updateEmployee.getId(),3);
+        assertEquals(updateEmployee.getName(),"Ricardo");
+        assertEquals(updateEmployee.getLastName(),"Castro");
+        assertEquals(updateEmployee.getDocument(),"257833");
+        assertEquals(updateEmployee.getUser(),"srcastrot");
+        assertEquals(updateEmployee.getPassword(),MainController.md5Security.MD5Security("ingesoft"));
+        assertTrue(updateEmployee.isAdministrator());
+        assertTrue(updateEmployee.isIsActive());
+    }
     
     @Test
-    public void updateAnEmployeeWithEmptyFields(){}
+    public void updateAnEmployeeWithEmptyFields(){
+        //New Data
+        setLastName("");
+        setPassword("castor");
+        setConfirmation("castor");
+        
+        Employee updateEmployee = employeeJpaController.findEmployee(Long.valueOf(3));
+        
+        assertEquals(updateEmployee.getId(),3);
+        assertEquals(updateEmployee.getName(),"Ricardo");
+        assertEquals(updateEmployee.getLastName(),"Castro");
+        assertEquals(updateEmployee.getDocument(),"257833");
+        assertEquals(updateEmployee.getUser(),"srcastrot");
+        assertEquals(updateEmployee.getPassword(),MainController.md5Security.MD5Security("ingesoft"));
+        assertTrue(updateEmployee.isAdministrator());
+        assertTrue(updateEmployee.isIsActive());
+        
+        // Old Data
+        setId(updateEmployee.getId());
+        setName(updateEmployee.getName());
+        setDocument(updateEmployee.getDocument());
+        setUser(updateEmployee.getUser());
+        setAdministrator(updateEmployee.isAdministrator());
+        setActive(updateEmployee.isIsActive());
+        
+        EmployeeManagementController.updateOldEmployee(getId(), getLastName(), getName(), getDocument(), getUser(), getPassword(), getConfirmation(), isActive(), isAdministrator());
+        
+        assertTrue(EmployeeManagementController.validatePasswords(getPassword(), getConfirmation()));
+        assertFalse(EmployeeManagementController.validateNotEmptyFields(getLastName(), getName(), getDocument(), getUser(), getPassword()));
+        
+        updateEmployee = employeeJpaController.findEmployee(Long.valueOf(3));
+        
+        assertEquals(updateEmployee.getId(),3);
+        assertEquals(updateEmployee.getName(),"Ricardo");
+        assertEquals(updateEmployee.getLastName(),"Castro");
+        assertEquals(updateEmployee.getDocument(),"257833");
+        assertEquals(updateEmployee.getUser(),"srcastrot");
+        assertEquals(updateEmployee.getPassword(),MainController.md5Security.MD5Security("ingesoft"));
+        assertTrue(updateEmployee.isAdministrator());
+        assertTrue(updateEmployee.isIsActive());
+    }
     
     @Test
-    public void updateAnEmployeeInvalidatingRules(){}
+    public void updateAnEmployeeInvalidatingRules(){
+        //New Data
+        setUser("Ingenieriadesoftware");
+        setActive(false);
+        setPassword("aponte");
+        setConfirmation("aponte");
+        
+        Employee updateEmployee = employeeJpaController.findEmployee(Long.valueOf(3));
+        
+        assertEquals(updateEmployee.getId(),3);
+        assertEquals(updateEmployee.getName(),"Ricardo");
+        assertEquals(updateEmployee.getLastName(),"Castro");
+        assertEquals(updateEmployee.getDocument(),"257833");
+        assertEquals(updateEmployee.getUser(),"srcastrot");
+        assertEquals(updateEmployee.getPassword(),MainController.md5Security.MD5Security("ingesoft"));
+        assertTrue(updateEmployee.isAdministrator());
+        assertTrue(updateEmployee.isIsActive());
+        
+        //Old Data
+        setId(updateEmployee.getId());
+        setName(updateEmployee.getName());
+        setLastName(updateEmployee.getLastName());
+        setDocument(updateEmployee.getDocument());
+        setAdministrator(updateEmployee.isAdministrator());
+        
+        EmployeeManagementController.updateOldEmployee(getId(), getLastName(), getName(), getDocument(), getUser(), getPassword(), getConfirmation(), isActive(), isAdministrator());
+        
+        assertTrue(EmployeeManagementController.validatePasswords(getPassword(), getConfirmation()));
+        assertTrue(EmployeeManagementController.validateNotEmptyFields(getLastName(), getName(), getDocument(), getUser(), getPassword()));
+        assertFalse(EmployeeManagementController.validateAll(getLastName(), getName(), getDocument(), getUser(), getPassword()));
+        
+        updateEmployee = employeeJpaController.findEmployee(Long.valueOf(3));
+        
+        assertEquals(updateEmployee.getId(),3);
+        assertEquals(updateEmployee.getName(),"Ricardo");
+        assertEquals(updateEmployee.getLastName(),"Castro");
+        assertEquals(updateEmployee.getDocument(),"257833");
+        assertEquals(updateEmployee.getUser(),"srcastrot");
+        assertEquals(updateEmployee.getPassword(),MainController.md5Security.MD5Security("ingesoft"));
+        assertTrue(updateEmployee.isAdministrator());
+        assertTrue(updateEmployee.isIsActive());
+    }
     
     // Class variables, getters and setters methods
     public boolean isActive() {
@@ -196,12 +485,30 @@ public class AdministerUserCaseTest {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public String getConfirmation() {
+        return confirmation;
+    }
+
+    public void setConfirmation(String confirmation) {
+        this.confirmation = confirmation;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
     
     private long id;
     private String lastName;
     private String name;
     private String document;
+    private String user;
     private String password;
+    private String confirmation;
     private boolean administrator;
     private boolean active;
     
