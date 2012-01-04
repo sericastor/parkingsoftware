f7bafd50afb91cdb593d0dd878893feb9274a16b
r4wd3r <r4wd3r@r4wd3r-PC>
2012-01-04 17:57:38 -0500

    Ahora si sirve :P

--- src/Entity/Facture.java
+++ src/Entity/Facture.java
@@ -11,6 +11,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
@@ -39,6 +40,8 @@ public class Facture implements Serializable {
     @OneToOne
     private FactureTurn factureTurn;
 
+    /*@OneToMany
+    private List<FactureTurn> facturesTurn = new ArrayList();*/
     //@OneToMany
     //private List<FactureTurn> facturesTurn = new ArrayList();
 
