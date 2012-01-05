3b74ee981adaeae472b4143de9db4d72ac4a142e
r4wd3r <r4wd3r@r4wd3r-PC>
2012-01-04 19:35:05 -0500

    Implementacion de metodos para las tablas de la vista

--- src/controller/CloseTurnController.java
+++ src/controller/CloseTurnController.java
@@ -18,6 +18,8 @@ import javax.swing.table.DefaultTableModel;
 public class CloseTurnController {
 
     public CloseTurnController() {
+        AllFactureTurn = MainController.factureTurnJpaController.findFactureTurnEntities();
+        AllFactures = MainController.factureJpaController.findFactureEntities();
     }
     public static int getNextFactureTurnID() {
         return MainController.factureTurnJpaController.getFactureTurnCount() + 1;
@@ -77,5 +79,46 @@ public class CloseTurnController {
         MainController.mainView.setExitsTableModel(exitsModel);*/
     }
     
+    public static DefaultTableModel TotalSearchOfFactureTurn() {
+        DefaultTableModel results = new DefaultTableModel();
+        AllFactureTurn = MainController.factureTurnJpaController.findFactureTurnEntities();
+        
+        results.addColumn("ID");
+        results.addColumn("Operario Entrada");
+        results.addColumn("Operario Salida");
+        results.addColumn("Placa");
+        results.addColumn("Fecha de Ingreso");
+        results.addColumn("Fecha de Salida");
+        results.addColumn("Subtotal");
+        results.addColumn("IVA");
+        results.addColumn("Total");
+        
+        for (FactureTurn e : AllFactureTurn) {
+            results.addRow( new Object []{String.valueOf(e.getId()),String.valueOf(e.getExit().getEmployeeEntry().getId()),String.valueOf(e.getExit().getEmployeeExit().getId()),e.getExit().getPlate(), e.getExit().getEntryDate().toLocaleString(),e.getExit().getExitDate().toLocaleString(), String.valueOf(e.getSubtotal()),String.valueOf(e.getIVA()),String.valueOf(e.getTotal())});
+        }
+        return results;
+    }
+    
+    public static DefaultTableModel TotalSearchOfFactures() {
+        DefaultTableModel results = new DefaultTableModel();
+        AllFactures = MainController.factureJpaController.findFactureEntities();
+        
+        results.addColumn("ID");
+        results.addColumn("Operario Entrada");
+        results.addColumn("Operario Salida");
+        results.addColumn("Placa");
+        results.addColumn("Fecha de Ingreso");
+        results.addColumn("Fecha de Salida");
+        results.addColumn("Subtotal");
+        results.addColumn("IVA");
+        results.addColumn("Total");
+        
+        for (Facture e : AllFactures) {
+            results.addRow( new Object []{String.valueOf(e.getId()),String.valueOf(e.getFactureTurn().getExit().getEmployeeEntry().getId()),String.valueOf(e.getFactureTurn().getExit().getEmployeeExit().getId()),e.getFactureTurn().getExit().getPlate(), e.getFactureTurn().getExit().getEntryDate().toLocaleString(),e.getFactureTurn().getExit().getExitDate().toLocaleString(), String.valueOf(e.getSubtotal()),String.valueOf(e.getIva()),String.valueOf(e.getTotal())});
+        }
+        return results;
+    }
+    private static List<Facture> AllFactures = null;
+    private static List<FactureTurn> AllFactureTurn = null;
     
 }
