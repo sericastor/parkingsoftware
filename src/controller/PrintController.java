f2bb26b5ec68683328dc0a3e955ccd52aeecc172
r4wd3r <r4wd3r@r4wd3r-PC>
2012-01-26 22:11:06 -0500

    Pretendiendo Imprimir Mostrando en Pantalla (Problema de paneles)

--- src/controller/PrintController.java
+++ src/controller/PrintController.java
@@ -4,11 +4,14 @@
  */
 package controller;
 
+import java.awt.Dimension;
 import java.awt.print.PrinterException;
 import java.awt.print.PrinterJob;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JFrame;
 import view.PreviewEntryTicket;
+import view.PrintFrame;
 
 /**
  *
@@ -19,15 +22,23 @@ public class PrintController {
     public PrintController() {
     }
     public static void printEntryTicket(PreviewEntryTicket ticket){
-        try {
-            PrinterJob job = PrinterJob.getPrinterJob();
-            job.setPrintable(ticket);
-            
-            boolean aceptar_impresion =job.printDialog();
-             if(aceptar_impresion)
-                job.print();
-        } catch (PrinterException ex) {
-            System.out.println("pedo");            
-        }
+        /*JFrame frame = new JFrame();
+        frame.add(ticket);
+        frame.setPreferredSize(new Dimension(600,600));
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.repaint();
+        frame.setVisible(true);
+        System.out.println(frame.getComponent(0).getName());*/
+        PrintFrame pf = new PrintFrame(); 
+        ticket.setVisible(true);
+        pf.setPreferredSize(ticket.getSize());
+        pf.getPrintPanel().removeAll();
+        pf.getPrintPanel().setPreferredSize(ticket.getPreferredSize());
+        pf.getPrintPanel().add(ticket).setVisible(true);
+        pf.setVisible(true);
+        
+                       
+        //pf.printMethod();
     }
 }
